1096a6c78f143c302a696bafea462151da1d58ef
Ripper <vivekkumar.62120@gmail.com>
2021-03-05 17:46:18 +0530

    hi

---
+++ String_Literals/script.js
@@ -0,0 +1,14 @@
+
+let s1 = 'He said, "he is awesome"'
+let s2 = "He said, \"he is awesome\""
+let s3 = "He said, 'he is awesome'"
+let s4 = `He said, "he is awesome"`
+
+let s5 = "this is in \n multiple lines"
+let s6 = `this is in
+ multiple lines`
+
+let name = 'John'
+let s7 = 'Good Morning ' + name
+let s8 = `Good Morning ${name}`
+let s9 = `10+2 = ${10+2}`
